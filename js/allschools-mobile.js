13ed66214c666520002abf24f65204354c6243bb
Michael Ploed <michael.ploed@gmail.com>
2012-05-01 00:06:14 +0200

    back button to news page, fixed icon naming (404 error)

--- js/allschools-mobile.js
+++ js/allschools-mobile.js
@@ -26,7 +26,7 @@ function getNewsList() {
 		$.each(news, function(index, entry) {
 			$('#newsList').append('<li data-role="list-divider">' + entry.date + ' <span class="ui-li-count">' + entry.count+ '</span></li>');
 			$.each(entry.news, function(index, entryDetail) {
-				$('#newsList').append('<li><a href="news.html?id=' + entryDetail.id + '"><h3>'+entryDetail.header+'</h3><p>'+entryDetail.teaser+'</p><p class="ui-li-aside">' + entryDetail.time + '</p></a></li>');
+				$('#newsList').append('<li><a data-transition="slide" href="news.html?id=' + entryDetail.id + '"><h3>'+entryDetail.header+'</h3><p>'+entryDetail.teaser+'</p><p class="ui-li-aside">' + entryDetail.time + '</p></a></li>');
 			});
 		});
 		$('#newsList').listview('refresh');
