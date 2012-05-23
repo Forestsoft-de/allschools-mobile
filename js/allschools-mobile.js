9428ac1c4f8f947853a079ffc30fc13665581048
Michael Ploed <michael.ploed@gmail.com>
2012-05-23 15:13:31 +0200

    changed json interface of newslist

--- js/allschools-mobile.js
+++ js/allschools-mobile.js
@@ -31,10 +31,9 @@ function getNews() {
 }
 
 function getNewsList() {
-	$.getJSON(serviceURL + 'newslist.json', function(data) {
+	$.getJSON('http://localhost:8080/rest/news?callback=?', function(data) {
 		$('#newsList li').remove();
-		var news = data.items;
-		$.each(news, function(index, entry) {
+		$.each(data, function(index, entry) {
 			$('#newsList').append('<li data-role="list-divider">' + entry.date + ' <span class="ui-li-count">' + entry.count+ '</span></li>');
 			$.each(entry.news, function(index, entryDetail) {
 				$('#newsList').append('<li><a data-transition="slide" href="news.html?id=' + entryDetail.id + '"><h3>'+entryDetail.header+'</h3><p>'+entryDetail.teaser+'</p><p class="ui-li-aside">' + entryDetail.time + '</p></a></li>');
